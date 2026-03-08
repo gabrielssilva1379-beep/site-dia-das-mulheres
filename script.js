7b3ea75366e0b63fba512a99f4f825650392826e
gabrielssilva1379-beep <gabriel.s.silva1379@gmail.com>
2026-03-08 00:42:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function createHeart(){
+
+const heart = document.createElement("div")
+
+heart.classList.add("heart")
+
+heart.innerHTML="❤️"
+
+heart.style.left=Math.random()*100+"vw"
+
+heart.style.animationDuration=(Math.random()*3+2)+"s"
+
+document.body.appendChild(heart)
+
+setTimeout(()=>{
+heart.remove()
+},5000)
+
+}
+
+setInterval(createHeart,300)
